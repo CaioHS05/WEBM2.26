635bdb87e3d9caa983bac2d30236f848e1e520ca
CaioHS05 <caio.n4pp@gmail.com>
2026-09-17 09:41:36 -0300

    Ver1

---
+++ README.md
@@ -0,0 +1,3 @@
+# WEBM2.26
+
+Repositório para acompanhamento das aulas da turma do segundo semestre de web
---
+++ main.js
@@ -0,0 +1,17 @@
+var Value1 = 25
+var Value2 = 25
+var Value3 = "Pizza"
+
+function plus(a,b){
+    let c = a+b
+    return (c)
+}
+
+console.log(plus(Value1,Value2))
+
+function pepperoni(c){
+    var Value3 = c
+    return (c)
+}
+
+console.log(pepperoni(Value3))
